9f370517a9e86b8f5088e1537ef0d8e46240986c
nombear <vijaynambiar1995@gmail.com>
2018-06-20 12:59:44 -0600

    logging final

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -30,14 +30,28 @@ public class Application {
         return new ServiceCredentials(vcapServices);
     }
 
+    private BlobStore getS3BlobStore(String s3AccessKey,
+                                     String s3SecretKey,
+                                     String s3BucketName,
+                                     String s3EndpointUrl) {
+        AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey,
+                s3SecretKey);
+        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+
+        if (s3EndpointUrl != null && !s3EndpointUrl.equals("")) {
+            s3Client.setEndpoint(s3EndpointUrl);
+        }
+
+        return new S3Store(s3Client, s3BucketName);
+    }
 
     @Profile("cloud")
     @Bean
-    public BlobStore cloudS3BlobStore(
+    public BlobStore blobStore(
             @Value("${vcap.services.moviefun-s3-service.credentials.access_key_id}") String s3AccessKey,
             @Value("${vcap.services.moviefun-s3-service.credentials.secret_access_key}") String s3SecretKey,
             @Value("${vcap.services.moviefun-s3-service.credentials.bucket}") String s3BucketName,
-            @Value("${vcap.services.moviefun-s3-service.credentials.s3_endpointurl}") String s3EndpointUrl
+            @Value("${vcap.services.moviefun-s3-service.credentials.s3_endpointurl:}") String s3EndpointUrl
     ) {
         return getS3BlobStore(s3AccessKey, s3SecretKey, s3BucketName, s3EndpointUrl);
     }
